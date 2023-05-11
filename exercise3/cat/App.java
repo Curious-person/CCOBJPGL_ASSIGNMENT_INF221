da7c399844da5fe9dc0aa4d035514e5b0a1fcd73
John Kevin Abgao <111432788+Curious-person@users.noreply.github.com>
2023-05-11 10:10:38 +0800

    Create App.java

---
+++ exercise3/cat/App.java
@@ -0,0 +1,13 @@
+package cat;
+
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        pet migato = new pet();
+
+        migato.meow();
+        System.out.println(migato.language);
+        System.out.println(migato.ability);
+    }
+  
+}
