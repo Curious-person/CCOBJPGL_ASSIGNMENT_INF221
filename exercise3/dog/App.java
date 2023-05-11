804b7eea73644cd3260cfe6a9e3b823d24b40dc6
John Kevin Abgao <111432788+Curious-person@users.noreply.github.com>
2023-05-11 10:13:29 +0800

    Create App.java

---
+++ exercise3/dog/App.java
@@ -0,0 +1,12 @@
+package dog;
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Dogzu miPerro = new Dogzu();
+
+        miPerro.bark();
+        System.out.println(miPerro.mouth);
+        System.out.println(miPerro.language);
+
+    }
+}
