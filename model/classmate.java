7d96442a47a7cd440bb6b6c0adf7c505dd9516be
John Kevin Abgao <111432788+Curious-person@users.noreply.github.com>
2023-05-25 11:45:08 +0800

    Add files via upload

---
+++ model/classmate.java
@@ -0,0 +1,23 @@
+package model;
+
+class classmate {
+
+    private String gender;
+    private String personality;
+
+    public String getgender() {
+        return this.gender;
+    }
+
+    public void setgender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getpersonality() {
+        return this.personality;
+    }
+
+    public void setpersonality(String personality) {
+        this.personality = personality;
+    }
+}
